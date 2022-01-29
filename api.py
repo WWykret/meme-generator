9045717d45a7c0dba731d7c5d8be61f61134315c
Wojciech Wykret <57493639+WWykret@users.noreply.github.com>
2022-01-29 15:55:30 +0100

    Added simple user remembering

--- api.py
+++ api.py
@@ -1,4 +1,6 @@
 from bridges import IUiBridge
+import json
+import os.path
 
 class ApiConnector(IUiBridge):
 
@@ -11,7 +13,9 @@ class ApiConnector(IUiBridge):
         self.password = password
         
         if save_credentials:
-            pass
+            data = {'username':username, 'password':password}
+            with open('credentials.json', 'w') as cred_file:
+                json.dump(data, cred_file)
 
         return True
 
@@ -19,4 +23,13 @@ class ApiConnector(IUiBridge):
         if hasattr(self, 'username') and hasattr(self, 'password'):
             return True
 
+        if os.path.isfile('credentials.json'):
+            with open('credentials.json', 'r') as cred_file:
+                data = json.load(cred_file)
+                try:
+                    self.username = data['username']
+                    self.password = data['password']
+                    return True
+                except KeyError:
+                    print('credentials file corrupted!')
         return False
