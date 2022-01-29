9847e30fa10246585f4f401bfdd0324a8da949aa
Wojciech Wykret <57493639+WWykret@users.noreply.github.com>
2022-01-29 12:53:27 +0100

    Added simple logging screen

--- ui.py
+++ ui.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import simpledialog
 
 class App:
     def __init__(self):
@@ -8,6 +9,26 @@ class App:
 
         tk.mainloop()
 
+    def get_credentials(self):
+        username_label = tk.Label(self.root, text="Username:")
+        username_label.grid(row=0, column=0, columnspan=1, stick='w', padx=10, pady=10)
+
+        username_entry = tk.Entry(self.root)
+        username_entry.grid(row=0, column=1, columnspan=2, stick='we', padx=10, pady=10)
+        
+        password_label = tk.Label(self.root, text="Password:")
+        password_label.grid(row=1, column=0, columnspan=1, stick='w', padx=10, pady=10)
+
+        password_entry = tk.Entry(self.root)
+        password_entry.grid(row=1, column=1, columnspan=2, stick='we', padx=10, pady=10)
+        
+        com = lambda: print(f'u: {username_entry.get()}; p: {password_entry.get()}')
+        submit_btn = tk.Button(self.root, text='Submit', command=com)
+        submit_btn.grid(row=3, column=0, columnspan=3, stick='we', padx=10, pady=10)
+        # com()
+
     def setup_ui(self):
-        canvas = tk.Canvas(self.root, width=400, height=400)
-        canvas.pack()
+        canvas = tk.Canvas(self.root)
+        canvas.grid(columnspan=3)
+
+        self.get_credentials()
